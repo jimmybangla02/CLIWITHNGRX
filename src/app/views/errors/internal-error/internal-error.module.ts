3988c7c48447ee34d0db456f3f98320fb761d393
S M Ashrafuzzaman <smashrafuzzaman@Jimmy-2.local>
2019-03-27 11:16:35 -0400

    add internal error component

---
+++ src/app/views/errors/internal-error/internal-error.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InternalErrorComponent } from './internal-error.component';
+
+@NgModule({
+  declarations: [InternalErrorComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class InternalErrorModule { }
